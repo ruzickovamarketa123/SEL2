3e6c93a2033e5cd6bc23980336d1adaecd1c2c8e
Serena <seregreg04@gmail.com>
2026-02-24 17:29:22 +0100

    Fron end component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchInput } from './search-input/search-input';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
