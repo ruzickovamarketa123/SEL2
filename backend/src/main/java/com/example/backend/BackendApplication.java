46dc25af4625c21d364aab51787b6564e8fc4dda
Your Name <your@email.com>
2026-04-09 17:21:36 +0200

    backend setup

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "world") String name) {
+        return String.format("hello %s!", name);
+    }
 }
