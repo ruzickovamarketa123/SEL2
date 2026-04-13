d2e61e33c082833a96932b17e65147f6b80b3b6d
Your Name <your@email.com>
2026-04-13 16:45:47 +0200

    backend up to dat

---
+++ backend/src/main/java/com/example/backend/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.TodoDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!!";
+    }
+
+    @GetMapping ("/test/todo")
+    public TodoDto testDto() {
+        return new TodoDto(1, "Shopping", false);
+    }
+}
