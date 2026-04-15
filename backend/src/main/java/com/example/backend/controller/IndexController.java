c3246db870de6d299358ec9dba85cecdb9a088e8
Your Name <your@email.com>
2026-04-15 12:59:58 +0200

    backend change

--- backend/src/main/java/com/example/backend/controller/IndexController.java
+++ backend/src/main/java/com/example/backend/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.TodoDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
