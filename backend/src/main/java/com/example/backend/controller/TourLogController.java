df24e27d4fe3f4c7a85a55403172e7b55ceab757
Your Name <your@email.com>
2026-04-16 16:29:52 +0200

    change url

--- backend/src/main/java/com/example/backend/controller/TourLogController.java
+++ backend/src/main/java/com/example/backend/controller/TourLogController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/tours/{tourId}/logs")
+@RequestMapping("/logs")
 public class TourLogController {
 
     // static list for now > to be implemented
