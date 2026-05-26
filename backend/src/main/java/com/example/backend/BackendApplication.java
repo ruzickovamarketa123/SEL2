a595aa56144ee004860d783c4219901d9436e42d
Serena <seregreg04@gmail.com>
2026-05-26 11:14:59 +0200

    merging

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,7 @@
 package com.example.backend;
 
-import com.example.backend.entity.Tour;
-import com.example.backend.repository.TourRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BackendApplication {
