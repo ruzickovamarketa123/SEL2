f83dbed4383e0e00a75435cd2a8b25b285edac2a
Your Name <your@email.com>
2026-07-05 23:38:31 +0200

    tododto deleted

--- backend/src/main/java/com/example/backend/controller/IndexController.java
+++ backend/src/main/java/com/example/backend/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.example.backend.controller;
 
-import com.example.backend.dto.TodoDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +12,5 @@ public class IndexController {
     public String hello() {
         return "Hello World!!!";
     }
-
-    // checks that DTO serialization works - that spring can correctly convert a Java object into JSON
-    @GetMapping("/test/todo")
-    public TodoDto testDto() {
-        return new TodoDto(1,"shopping", false);
-    }
-
+    
 }
